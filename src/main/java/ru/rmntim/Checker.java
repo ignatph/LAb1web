799aac12f37bc5ae3cc5c8d4489b287d42f158f2
ignatpahomov <ignatpaxomov@gmail.com>
2025-10-17 17:44:03 +0300

    teper tochno tochno last last

--- src/main/java/ru/rmntim/Checker.java
+++ src/main/java/ru/rmntim/Checker.java
@@ -12,8 +12,6 @@ public class Checker {
 
 
     private static boolean checkTriangle(float x, float y, float r) {
-        // Triangle with vertices (0,0), (-R,0), (0,R/2) in Q2
-        // Inside condition: x <= 0, y >= 0, and y <= x/2 + R/2
         return x <= 0 && y >= 0 && y <= (x / 2f) + (r / 2f);
     }
 
